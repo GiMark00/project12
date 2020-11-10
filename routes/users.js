3fd691bd86d94a3391d124dafd4fada6d788c92f
GiMark00 <gimarko@yandex.ru>
2020-11-10 17:21:03 +0300

    v.0.0.3

--- routes/users.js
+++ routes/users.js
@@ -1,37 +1,37 @@
-const PostUsers = require("express").Router();
-const fs = require("fs").promises;
+const PostUsers = require('express').Router();
+const fs = require('fs').promises;
 
 const SingleUser = (req, res) => {
-  fs.readFile("./data/users.json", "utf-8")
+  fs.readFile('./data/users.json', 'utf-8')
     .then((data) => {
       const users = JSON.parse(data);
       const userId = users.find((item) => item._id === req.params.id);
 
       if (!userId) {
-        res.status(500).send({ message: "Нет пользователя с таким id" });
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
         return;
       }
 
       res.status(200).json(userId);
     })
-    .catch((err) => {
-      res.status(500).json({ message: `Ошибка при чтении файла: ${err}` });
+    .catch(() => {
+      res.status(500).json({ message: 'Ошибка при чтении файла.' });
     });
 };
 
 const AllUsers = (req, res) => {
-  fs.readFile("./data/users.json", "utf-8")
+  fs.readFile('./data/users.json', 'utf-8')
     .then((data) => {
       let users = { ...data };
       users = JSON.parse(data);
       res.status(200).json(users);
     })
-    .catch((err) => {
-      res.status(500).json({ message: `Ошибка при чтении файла: ${err}` });
+    .catch(() => {
+      res.status(500).json({ message: 'Ошибка при чтении файла.' });
     });
 };
 
-PostUsers.get("/users/:id", SingleUser);
-PostUsers.get("/users", AllUsers);
+PostUsers.get('/users/:id', SingleUser);
+PostUsers.get('/users', AllUsers);
 
 module.exports = PostUsers;
